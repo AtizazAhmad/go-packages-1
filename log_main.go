184a984a053f4669701e225d142e417c73531874
Your Name <ayeshazakria22@gmail.com>
2020-12-26 08:27:57 -0800

    logging main ftn

---
+++ log_main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/FaizanMajid1/go-packages/logging"
+
+func main() {
+    logging.Debug(true)
+
+    logging.Log("This is a debug statement...")
+
+  //  fmt.Println(logging.debug)
+}
+
