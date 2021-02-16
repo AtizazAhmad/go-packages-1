1f20608997fd1afaa0e42d6d37347984d29eba9e
Your Name <ayeshazakria22@gmail.com>
2021-02-15 22:38:25 -0800

    adding change to conv2.go

--- tempconv/conv2.go
+++ tempconv/conv2.go
@@ -1,6 +1,8 @@
+package tempconv
+
 func CtoF(c Celsius) Fahrenheit{
- return  Fahrenheit(c*9/5+32)}
+  return  Fahrenheit(c*9/5+32)}
 
 func FtoC(f Fahrenheit) Celsius{
- return Celsius((f-32)*5/9)
+  return Celsius((f-32)*5/9)
 }
